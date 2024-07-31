68d6c6939c05771af9e8f8c971e88c6f30c3659c
Daring Ace <andrew.berryhill2005@gmail.com>
2024-07-31 15:14:49 -0500

    7/31/2024 - Additions & Replacements

--- assets/preload/data/Grand-Finale/misty.lua
+++ assets/preload/data/Grand-Finale/misty.lua
@@ -1,14 +1,14 @@
 function onCreate()
-  makeAnimatedLuaSprite('misty', 'misty', -500, -400)
+  makeAnimatedLuaSprite('misty', 'misty', -750, -750)
   addAnimationByPrefix('misty', 'idle', 'neblina instancia 1', 24, true)
-  scaleObject('misty', 11, 11)
+  scaleObject('misty', 13, 13)
   setProperty('misty.alpha', 0)
   addLuaSprite('misty', true)
 end
 
 onEvent = function(n,v1,v2)
   if n == 'Lights out' then
-    doTweenAlpha('fard', 'misty', 0.75, 0.75, 'ease')
+    doTweenAlpha('fard', 'misty', 0.825, 0.75, 'ease')
     playAnim("misty", "idle", true, false, 0)
   end
   if n == 'Lights on' then
